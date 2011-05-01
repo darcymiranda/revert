5edc54e9095dec650f114348ae31c68fd83c9043
kalkitus <darcymir@gmail.com>
2011-05-01 11:56:28 -0500

    added interpolation to ships and client/server bullets

---
+++ src/client/revert/EntityState.java
@@ -0,0 +1,39 @@
+package client.revert;
+
+import org.newdawn.slick.geom.Vector2f;
+
+public class EntityState {
+	
+	private Vector2f position;
+	private Vector2f velocity;
+	
+	private float rotation;
+	
+	public EntityState(Entity entity){
+		position = entity.getClientPosition();
+		velocity = entity.getVelocity();
+		rotation = entity.getRotation();
+	}
+	
+	public void update(){
+		
+		position.x += velocity.x;
+		position.y -= velocity.y;
+		
+	}
+	
+	public void setState(EntityState state){
+		position = state.getPosition();
+		velocity = state.getVelocity();
+		rotation = state.getRotation();
+	}
+
+	public void setPosition(float x, float y){ position.x = x; position.y = y; }
+	public void setVelocity(float x, float y){ velocity.x = x; velocity.y = y; }
+	public void setRotation(float r){ rotation = r; }
+	
+	public Vector2f getPosition(){ return new Vector2f(position); }
+	public Vector2f getVelocity(){ return new Vector2f(velocity); }
+	public float getRotation(){ return rotation; }
+
+}
